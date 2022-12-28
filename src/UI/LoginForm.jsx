12fc871a03bb35fc5ce89537a7f2693e0d844211
Anupam Sushil <anupam.sushil1@gmail.com>
2022-12-28 02:22:57 -0500

    small bug

--- src/UI/LoginForm.jsx
+++ src/UI/LoginForm.jsx
@@ -21,7 +21,7 @@ const Form = ({ users, login }) => {
     
     if (info.userName.length <= 1 || info.password.length <= 1) return;
     console.log("signing in", info)
-    axios.post(`http://localhost:3000/users`, info);
+    axios.post(`https://mathpreper.onrender.com/users`, info);
     login(info.userName);
       
   }
